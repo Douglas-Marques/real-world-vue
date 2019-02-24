c18d7d75edf07802c4af55f2dbe2a5d88f1ac927
Douglas-Marques <douglasmarques09@gmail.com>
2019-02-24 13:12:24 -0300

    Adding Vue Modules

---
+++ src/store/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as user from '@/store/modules/user.js'
+import * as event from '@/store/modules/event.js'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    user,
+    event
+  },
+  state: {
+    categories: [
+      'Sustainability',
+      'Nature',
+      'Animal Welfare',
+      'Housing',
+      'Education',
+      'Food',
+      'Community'
+    ]
+  }
+})
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -57,7 +57,7 @@ export default {
   methods: {
     createEvent() {
       this.$store
-        .dispatch('createEvent', this.event)
+        .dispatch('event/createEvent', this.event)
         .then(() => {
           this.$router.push({
             name: 'event-show',
@@ -70,8 +70,9 @@ export default {
         })
     },
     createFreshEventObject() {
-      const user = this.$store.state.user
+      const user = this.$store.state.user.user
       const id = Math.floor(Math.random() * 1000000)
+
       return {
         id: id,
         user: user,
