1a8c2d0c7727959524cbedd1db01fbb31226961e
Douglas-Marques <douglasmarques09@gmail.com>
2019-02-24 16:46:01 -0300

    Adding progress bar

--- src/router.js
+++ src/router.js
@@ -3,16 +3,19 @@ import Router from 'vue-router'
 import EventCreate from './views/EventCreate.vue'
 import EventList from './views/EventList.vue'
 import EventShow from './views/EventShow.vue'
+import NProgress from 'nprogress'
+import store from '@/store/store'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
       name: 'event-list',
-      component: EventList
+      component: EventList,
+      props: true
     },
     {
       path: '/event/create',
@@ -23,7 +26,24 @@ export default new Router({
       path: '/event/:id',
       name: 'event-show',
       component: EventShow,
-      props: true
+      props: true,
+      beforeEnter(routerTo, routerFrom, next) {
+        store.dispatch('event/fetchEvent', routerTo.params.id).then(event => {
+          routerTo.params.event = event
+          next()
+        })
+      }
     }
   ]
 })
+
+router.beforeEach((routerTo, routerFrom, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
+
+export default router
